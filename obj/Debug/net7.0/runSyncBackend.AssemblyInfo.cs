f17912ba4558b2a8d97d5971284f6c1bf67c6388
Janm20 <misterforce1@gmail.com>
2025-07-26 00:23:02 +0200

    setup for mac mongo

--- obj/Debug/net7.0/runSyncBackend.AssemblyInfo.cs
+++ obj/Debug/net7.0/runSyncBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("runSyncBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f5a4611c14bf62fb4cce6e392ce06541bfac10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("runSyncBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("runSyncBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net9.0/runSyncBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/runSyncBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("runSyncBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42b246a3e0074daa55802667d16e60131beedd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c96f67ccf5bfd1fac0b5fa29dae5df483ccd6bf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("runSyncBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("runSyncBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
